65f4819bd93bf331762cce2bcb9c0b5678001c77
Andrey Drenski <andreshko.dre@gmail.com>
2018-06-14 22:45:24 +0300

    useful return type for Union & more comments

--- union_find.h
+++ union_find.h
@@ -1,6 +1,6 @@
 #pragma once
 #include <vector>
-#include <type_traits>
+#include <type_traits> // std::is_unsigned_v
 
 template <class Idx>
 class union_find
@@ -9,10 +9,12 @@ class union_find
     struct pair { Idx p, r; };
     std::vector<pair> values;
     
+    // Ease of access, will be inlined
     Idx& parent(Idx x) { return values[x].p; }
     Idx&   rank(Idx x) { return values[x].r; }
 
-    Idx Find(Idx x) { // path halving
+    // Find w/ path halving. May probably be optimized a little more.
+    Idx Find(Idx x) {
         while (parent(x) != x) {
             parent(x) = parent(parent(x));
             x = parent(x);
@@ -20,21 +22,24 @@ class union_find
         return x;
     }
 public:
-    union_find(const Idx n) : values(n) {
+    explicit union_find(const Idx n) : values{ n } {
         for (Idx i = 0; i < n; ++i) {
             parent(i) = i;
             rank(i) = 0;
         }
     }
-    void Union(Idx x, Idx y) { // union by rank
+    // Union by rank. Returns true on successful union,
+    // false if x and y are already in the same component.
+    bool Union(Idx x, Idx y) {
         Idx xroot = Find(x), yroot = Find(y);
         if (xroot == yroot)
-            return;
+            return false;
         if (rank(xroot) < rank(yroot))
             std::swap(xroot, yroot);
         parent(yroot) = xroot;
         Idx& xrank = rank(xroot);
         if (xrank == rank(yroot))
             ++xrank;
+        return true;
     }
 };
